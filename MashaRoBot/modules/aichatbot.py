49804abc99a85d054dec11afb60ccd33c1a79ada
chrisdroid1 <82050423+chrisdroid1@users.noreply.github.com>
2021-06-27 17:10:57 +0100

    Update aichatbot.py

--- MashaRoBot/modules/aichatbot.py
+++ MashaRoBot/modules/aichatbot.py
@@ -11,7 +11,7 @@ url = "https://acobot-brainshop-ai-v1.p.rapidapi.com/get"
 
 translator = google_translator()
 
-BOT_ID = 1777728008
+BOT_ID = 1886379359
 
 def extract_emojis(s):
     return "".join(c for c in s if c in emoji.UNICODE_EMOJI)
