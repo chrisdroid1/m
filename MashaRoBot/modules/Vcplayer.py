b2fd79c3ba3643b819ffa02c4a36b7241fd6b48e
chrisdroid1 <82050423+chrisdroid1@users.noreply.github.com>
2021-06-27 12:01:13 +0100

    Update Vcplayer.py

--- MashaRoBot/modules/Vcplayer.py
+++ MashaRoBot/modules/Vcplayer.py
@@ -50,7 +50,7 @@ channel is also can be used instead of c ( /cplay = /channelplay )
 1) Get your channel ID.
 2) Create a group with tittle: Channel Music: your_channel_id
 3) Add bot as Channel admin with full perms
-4) Add @{ASSISTANT_NAME} to the channel as an admin.
+4) Add @Suchabusyguy to the channel as an admin.
 5) Simply send commands in your group.
 """,
 
