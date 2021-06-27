f9af13edbc28921c5ddde61585de0b2bad23e31e
chrisdroid1 <82050423+chrisdroid1@users.noreply.github.com>
2021-06-27 18:05:54 +0100

    Create _tools.py

---
+++ MashaRoBot/modules/_tools.py
@@ -0,0 +1,10 @@
+__mod_name__ = "Chatbot 🙋‍♀️"
+
+__help__ = """
+Chatbot utilizes the Upgraded API and allows Kaela to talk and provides a more interactive group chat experience.
+*Commands:* 
+*Admins only:*
+ ✪ `/addchat`*:* Enables Chatbot mode in the chat.
+ ✪ `/rmchat`*:* Disables Chatbot mode in the chat.
+Reports bugs at @{SUPPORT_CHAT}
+"""
