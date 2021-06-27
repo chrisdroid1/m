ab799aa3d13fc6a670923c80ff4d69899dd9940a
chrisdroid1 <82050423+chrisdroid1@users.noreply.github.com>
2021-06-27 18:56:44 +0100

    Update mongo.py

--- MashaRoBot/helper_extra/mongo.py
+++ MashaRoBot/helper_extra/mongo.py
@@ -12,11 +12,6 @@ MONGO_URI = os.environ.get("MONGO_URI")
 MONGO_PORT = os.environ.get("MONGO_PORT")
 MONGO_DB = os.environ.get("MONGO_DB")
 
-MONGO_PORT = get_int_key("27017")
-MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "DaisyX"
-
-
 
 # Init MongoDB
 mongodb = MongoClient(MONGO_URI, MONGO_PORT)[MONGO_DB]
