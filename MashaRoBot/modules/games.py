07ec09ef5b501fabd1704dd9606c742d6ada6a49
chrisdroid1 <82050423+chrisdroid1@users.noreply.github.com>
2021-06-28 21:12:21 +0100

    Create games.py

---
+++ MashaRoBot/modules/games.py
@@ -0,0 +1,48 @@
+from telethon.tl.types import InputMediaDice
+
+from MashaRoBot.events import register
+
+
+@register(pattern="^/dice(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    r = await event.reply(file=InputMediaDice(""))
+    input_int = int(input_str)
+    if input_int > 6:
+        await event.reply("hey nigga use number 1 to 6 only")
+
+
+@register(pattern="^/dart(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    r = await event.reply(file=InputMediaDice("🎯"))
+    input_int = int(input_str)
+    if input_int > 6:
+        await event.reply("hey nigga use number 1 to 6 only")
+
+
+@register(pattern="^/ball(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    r = await event.reply(file=InputMediaDice("🏀"))
+    input_int = int(input_str)
+    if input_int > 5:
+        await event.reply("hey nigga use number 1 to 6 only")
+
+
+__help__ = """
+ *Play Game With Emojis:*
+  
+  ✮ /dice σя /dice 1 тσ 6 αηу ναℓυє
+ 
+  ✮ /ball σя /ball 1 тσ 5 αηу ναℓυє
+  ✮ /dart σя /dart 1 тσ 6 αηу ναℓυє
+       
+
+__mod_name__ = "Gᴀᴍᴇꜱ🎮"
