12f8a3266ef1fcdeba32965ce80528d5c3e6f3ee
chrisdroid1 <82050423+chrisdroid1@users.noreply.github.com>
2021-07-03 04:47:34 +0100

    Update imgeditor.py

--- MashaRoBot/modules/imgeditor.py
+++ MashaRoBot/modules/imgeditor.py
@@ -450,12 +450,10 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
 __mod_name__ = "Image Editor"
 __help__ = """
-<b> IMAGE EDITOR </b>
-Daisy have some advanced image editing tools inbuilt
+• IMAGE EDITOR 
+Kaela have some advanced image editing tools inbuilt
 Bright, Circle, RemBG, Blur, Border, Flip, Glitch, Sticker maker and more
 
 - /edit [reply to image]: Open the image editor
 - /rmbg [REPLY]: Revove BG of replied image/sticker.
-
-<i> Special credits to TroJanzHEX </i>
 """
