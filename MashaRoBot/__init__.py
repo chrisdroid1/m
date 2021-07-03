22bb1f5003654a566b43032449b27ef36e8571c7
chrisdroid1 <82050423+chrisdroid1@users.noreply.github.com>
2021-07-03 05:16:19 +0100

    Update __init__.py

--- MashaRoBot/__init__.py
+++ MashaRoBot/__init__.py
@@ -77,7 +77,7 @@ if ENV:
     OPENWEATHERMAP_ID = os.environ.get("OPENWEATHERMAP_ID", None)
     VIRUS_API_KEY = os.environ.get("VIRUS_API_KEY", None)
     BOT_ID = int(os.environ.get("BOT_ID", None))
-    REM_BG_API_KEY = int(os.environ.get("REM_BG_API_KEY", None))
+    REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
     LOAD = os.environ.get("LOAD", "").split()
     NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
     DEL_CMDS = bool(os.environ.get("DEL_CMDS", False))
