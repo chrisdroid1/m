1a710cfb0b8f18d8d049eea07873dfc2446d58fe
chrisdroid1 <82050423+chrisdroid1@users.noreply.github.com>
2021-07-14 10:55:27 +0100

    Update aichatbot.py

--- MashaRoBot/modules/aichatbot.py
+++ MashaRoBot/modules/aichatbot.py
@@ -1,7 +1,7 @@
 import aiohttp
 import asyncio
 import re
-from config import bot_token, owner_id, bot_id
+from MashaRoBot import bot_token, owner_id, bot_id
 from pyrogram import Client, filters
 
 
