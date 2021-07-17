a82444dc005a05d2ed92783dbfe6d01815b26382
chrisdroid1 <82050423+chrisdroid1@users.noreply.github.com>
2021-07-18 00:28:52 +0100

    Update mongo.py

--- MashaRoBot/mongo.py
+++ MashaRoBot/mongo.py
@@ -3,20 +3,20 @@ import asyncio
 import sys
 
 from motor import motor_asyncio
-from MashaRoBot import MONGO_DB_URI 
+from MashaRoBot import MONGO_URI 
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
 from MashaRoBot.conf import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
-MONGO_DB_URI = get_str_key("MONGO_DB_URI")
+MONGO_URI = get_str_key("MONGO_DB_URI")
 MONGO_DB = "DaisyX"
 
 
 client = MongoClient()
-client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
-motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
+client = MongoClient(MONGO_URI, MONGO_PORT)[MONGO_DB]
+motor = motor_asyncio.AsyncIOMotorClient(MONGO_URI, MONGO_PORT)
 db = motor[MONGO_DB]
 db = client["yonerobot"]
 try:
