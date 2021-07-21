a02f44313db17c50d07ef5ac407c62cc7bd3a3c2
chrisdroid1 <82050423+chrisdroid1@users.noreply.github.com>
2021-07-21 02:50:28 +0100

    Update blacklistusers.py

--- MashaRoBot/modules/blacklistusers.py
+++ MashaRoBot/modules/blacklistusers.py
@@ -23,14 +23,13 @@ from telegram.utils.helpers import mention_html
 from MashaRoBot import MONGO_URI 
 from MashaRoBot.conf import get_int_key, get_str_key
 from pymongo import MongoClient
+
 MONGO_URI = get_str_key("MONGO_URI")
 
-
 client = MongoClient()
-
 client = MongoClient(MONGO_URI)
-
 db = client["bl"]
+
 
 BLACKLISTWHITELIST = [OWNER_ID] + DEV_USERS + DRAGONS + WOLVES + DEMONS
 BLABLEUSERS = [OWNER_ID] + DEV_USERS
