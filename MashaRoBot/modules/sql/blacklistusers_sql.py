ee385a45835ce871d037b3427f2d7a4782fc78d5
Christopher <82050423+chrisdroid1@users.noreply.github.com>
2021-07-22 22:19:48 +0100

    Update blacklistusers_sql.py

--- MashaRoBot/modules/sql/blacklistusers_sql.py
+++ MashaRoBot/modules/sql/blacklistusers_sql.py
@@ -1,68 +1,68 @@
 import threading
 
- from MashaRoBot.modules.sql import BASE, SESSION
- from sqlalchemy import Column, String, UnicodeText
+from MashaRoBot.modules.sql import BASE, SESSION
+from sqlalchemy import Column, String, UnicodeText
 
 
- class BlacklistUsers(BASE):
-     __tablename__ = "blacklistusers"
-     user_id = Column(String(14), primary_key=True)
-     reason = Column(UnicodeText)
+class BlacklistUsers(BASE):
+    __tablename__ = "blacklistusers"
+    user_id = Column(String(14), primary_key=True)
+    reason = Column(UnicodeText)
 
-     def __init__(self, user_id, reason=None):
-         self.user_id = user_id
-         self.reason = reason
-
-
- BlacklistUsers.__table__.create(checkfirst=True)
-
- BLACKLIST_LOCK = threading.RLock()
- BLACKLIST_USERS = set()
+    def __init__(self, user_id, reason=None):
+        self.user_id = user_id
+        self.reason = reason
 
 
- def blacklist_user(user_id, reason=None):
-     with BLACKLIST_LOCK:
-         user = SESSION.query(BlacklistUsers).get(str(user_id))
-         if not user:
-             user = BlacklistUsers(str(user_id), reason)
-         else:
-             user.reason = reason
+BlacklistUsers.__table__.create(checkfirst=True)
 
-         SESSION.add(user)
-         SESSION.commit()
-         __load_blacklist_userid_list()
+BLACKLIST_LOCK = threading.RLock()
+BLACKLIST_USERS = set()
 
 
- def unblacklist_user(user_id):
-     with BLACKLIST_LOCK:
-         user = SESSION.query(BlacklistUsers).get(str(user_id))
-         if user:
-             SESSION.delete(user)
+def blacklist_user(user_id, reason=None):
+    with BLACKLIST_LOCK:
+        user = SESSION.query(BlacklistUsers).get(str(user_id))
+        if not user:
+            user = BlacklistUsers(str(user_id), reason)
+        else:
+            user.reason = reason
 
-         SESSION.commit()
-         __load_blacklist_userid_list()
-
-
- def get_reason(user_id):
-     user = SESSION.query(BlacklistUsers).get(str(user_id))
-     rep = ""
-     if user:
-         rep = user.reason
-
-     SESSION.close()
-     return rep
+        SESSION.add(user)
+        SESSION.commit()
+        __load_blacklist_userid_list()
 
 
- def is_user_blacklisted(user_id):
-     return user_id in BLACKLIST_USERS
+def unblacklist_user(user_id):
+    with BLACKLIST_LOCK:
+        user = SESSION.query(BlacklistUsers).get(str(user_id))
+        if user:
+            SESSION.delete(user)
+
+        SESSION.commit()
+        __load_blacklist_userid_list()
 
 
- def __load_blacklist_userid_list():
-     global BLACKLIST_USERS
-     try:
-         BLACKLIST_USERS = {int(x.user_id) for x in SESSION.query(BlacklistUsers).all()}
-     finally:
-         SESSION.close()
+def get_reason(user_id):
+    user = SESSION.query(BlacklistUsers).get(str(user_id))
+    rep = ""
+    if user:
+        rep = user.reason
+
+    SESSION.close()
+    return rep
 
 
- __load_blacklist_userid_list()
+def is_user_blacklisted(user_id):
+    return user_id in BLACKLIST_USERS
+
+
+def __load_blacklist_userid_list():
+    global BLACKLIST_USERS
+    try:
+        BLACKLIST_USERS = {int(x.user_id) for x in SESSION.query(BlacklistUsers).all()}
+    finally:
+        SESSION.close()
+
+
+__load_blacklist_userid_list()
